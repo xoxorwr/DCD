02ee76cf0db1783448329718962ce5f0684e1721
xoxorwr <alien.frog.apocalypse@gmail.com>
2026-02-23 15:44:17 +0100

    Update tests

--- tests/tc_templates/file.d
+++ tests/tc_templates/file.d
@@ -12,8 +12,23 @@ struct Vec2{
     float y;
 }
 
-void main() {
+void test1() {
     Data!(Outter.Inner) mydata;
-
     mydata.;
+}
+void test2() {
+    Data!(Vec2) mydata;
+    mydata.;
+}
+
+
+T get_t(T)() {}
+
+void test3() {
+    auto mydata = get_t!(Outter.Inner);
+    mydat.;
+}
+void test4() {
+    auto mydata = get_t!(Vec2);
+    mydat.;
 }
--- tests/tc_templates/run.sh
+++ tests/tc_templates/run.sh
@@ -1,5 +1,18 @@
 set -e
 set -u
 
-../../bin/dcd-client $1 file.d -c 173  > actual.txt
-diff actual.txt expected.txt --strip-trailing-cr
+echo "-- 1"
+../../bin/dcd-client $1 file.d -c 173  > actual1.txt
+diff actual1.txt expected1.txt --strip-trailing-cr
+
+echo "-- 2"
+../../bin/dcd-client $1 file.d -c 227  > actual2.txt
+diff actual2.txt expected2.txt --strip-trailing-cr
+
+echo "-- 3"
+../../bin/dcd-client $1 file.d -c 316  > actual3.txt
+diff actual3.txt expected3.txt --strip-trailing-cr
+
+echo "-- 4"
+../../bin/dcd-client $1 file.d -c 378  > actual4.txt
+diff actual4.txt expected4.txt --strip-trailing-cr
